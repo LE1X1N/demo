001861f2650cae9b2ebac8afc6bd37b7adfa8302
xin.lei <realleixin@qq.com>
2025-09-01 17:34:24 +0800

    source code of q learning

---
+++ source/unit2/q_learning.py
@@ -0,0 +1,14 @@
+from pyvirtualdisplay import Display
+
+virtual_display = Display(visible=0, size=(1400, 900))
+virtual_display.start()
+
+import numpy as np
+import gymnasium as gym
+import random
+import imageio
+import os
+import tqdm
+
+import pickle5 as pickle
+from tqdm.notebook import tqdm
