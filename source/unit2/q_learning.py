811d8862346242876e3c14d3ea5b7f35d04f4c5d
xin.lei <realleixin@qq.com>
2025-09-03 18:08:37 +0800

    create frozenlake env

--- source/unit2/q_learning.py
+++ source/unit2/q_learning.py
@@ -12,3 +12,25 @@ import tqdm
 
 # import pickle5 as pickle  # not support for python >= 3.8
 from tqdm.notebook import tqdm
+
+"""
+Frozen Lake
+    Navigate from starting state (S) to the goal state (G) by walking on the frozen tiles (F) 
+    and avoid holes (H)
+
+    one desc could be :
+    
+    "4x4":[
+        "SFFF",
+        "FHFH",
+        "FFFH",
+        "HFFG"
+        ]
+"""
+
+# create a FrozenLake-v1 environment  
+env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False, render_mode="rgb_array")
+
+print("_____OBSERVATION SPACE______\n")
+print("Observation Space", env.observation_space)
+print("Sample observation", env.observation_space.sample())
