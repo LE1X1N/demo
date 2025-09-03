c7a0f413aab984a477278fe3e4aebfff6b2ea63b
xin.lei <realleixin@qq.com>
2025-09-03 21:34:20 +0800

    restructure package

--- src/unit2/q_learning.py
+++ src/unit2/q_learning.py
@@ -4,7 +4,7 @@ virtual_display = Display(visible=0, size=(1400, 900))
 virtual_display.start()
 
 import numpy as np
-import gymnasium as gym
+
 import random
 import imageio
 import os
@@ -14,49 +14,12 @@ import tqdm
 from tqdm import tqdm
 
 """
-Frozen Lake
-    Navigate from starting state (S) to the goal state (G) by walking on the frozen tiles (F) 
-    and avoid holes (H)
-
-    rewards:
-        Reach goal: +1
-        Reach hole: 0
-        Reach frozen: 0
-
-    one desc could be :
-    
-    "4x4":[
-        "SFFF",
-        "FHFH",
-        "FFFH",
-        "HFFG"
-        ]
-"""
-
-# create a FrozenLake-v1 environment  
-env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False, render_mode="rgb_array")
-
-print("\n_____OBSERVATION SPACE______")
-print("Observation Space", env.observation_space)
-print("Sample observation", env.observation_space.sample())
-
-print("\n_____ACTION SPACE_____")
-print("Action Space Shape", env.action_space.n)
-print("Action Space Sample", env.action_space.sample())
-
-"""
     initialzie q-table
 """
 def initialize_q_table(state_space, action_space):
     Qtable = np.zeros((state_space, action_space))
     return Qtable
 
-state_space = env.observation_space.n
-action_space = env.action_space.n
-print(f"There are {state_space} possible states")
-print(f"There are {action_space} possible actions")
-
-Qtable_frozenlake = initialize_q_table(state_space, action_space)
 
 """
     Greedy policy
@@ -77,31 +40,12 @@ def epsilon_greedy_policy(Qtable, state, epsilon):
         action = np.argmax(Qtable[state])
     return action
 
-"""
-    Hyper-parameters
-"""
-# training parameters
-n_training_episodes = 10000
-learning_rate = 0.7
 
-# evaluation parameters
-n_eval_episodes = 100
-
-# environment parameters
-env_id = "FrozenLake-v1"
-max_steps = 99
-gamma = 0.95
-eval_seed = []
-
-# exploration parameters
-max_epsilon = 1.0
-min_epsilon = 0.05
-decay_rate = 0.0005
 
 """
     Training Loop
 """
-def train(n_training_episodes, min_epsilon, max_epsilon, decay_rate, env, max_steps, Qtable):
+def train(n_training_episodes, min_epsilon, max_epsilon, decay_rate, env, max_steps, Qtable, learning_rate, gamma):
     
     epsilon = max_epsilon
 
@@ -128,8 +72,6 @@ def train(n_training_episodes, min_epsilon, max_epsilon, decay_rate, env, max_st
     
     return Qtable
 
-# training the Q-table 
-Qtable_frozenlake = train(n_training_episodes, min_epsilon, max_epsilon, decay_rate, env, max_steps, Qtable_frozenlake)
 
 """
     Evaluation method
@@ -163,6 +105,3 @@ def evaluate_agent(env, max_steps, n_eval_episodes, Q, seed):
     mean_reward = np.mean(episode_rewards)
     std_reward = np.std(episode_rewards)
     return mean_reward, std_reward
-
-mean_reward, std_reward = evaluate_agent(env, max_steps, n_eval_episodes, Qtable_frozenlake, eval_seed)
-print(f"Mean reward={mean_reward} +/- {std_reward}")
