d4b4b2438f32d8ffb0a321c013755075ef3d307b
xin.lei <realleixin@qq.com>
2025-09-03 21:11:25 +0800

    add eval method

--- source/unit2/q_learning.py
+++ source/unit2/q_learning.py
@@ -18,6 +18,11 @@ Frozen Lake
     Navigate from starting state (S) to the goal state (G) by walking on the frozen tiles (F) 
     and avoid holes (H)
 
+    rewards:
+        Reach goal: +1
+        Reach hole: 0
+        Reach frozen: 0
+
     one desc could be :
     
     "4x4":[
@@ -126,4 +131,38 @@ def train(n_training_episodes, min_epsilon, max_epsilon, decay_rate, env, max_st
 # training the Q-table 
 Qtable_frozenlake = train(n_training_episodes, min_epsilon, max_epsilon, decay_rate, env, max_steps, Qtable_frozenlake)
 
-print(1)
+"""
+    Evaluation method
+"""
+def evaluate_agent(env, max_steps, n_eval_episodes, Q, seed):
+    episode_rewards = []
+
+    for episode in tqdm(range(n_eval_episodes)):
+        if seed:
+            state, info = env.reset(seed=seed[episode])
+        else:
+            state, info = env.reset()
+        step = 0
+        truncated = False
+        terminated = False
+        total_rewards_ep = 0
+
+        for step in range(max_steps):
+            # acting policy
+            action = greedy_policy(Q, state)
+            new_state, reward, terminated, truncated, info = env.step(action)
+            total_rewards_ep += reward
+
+            if terminated or truncated:
+                break
+            state = new_state
+
+        episode_rewards.append(total_rewards_ep)
+    
+    # mean and std
+    mean_reward = np.mean(episode_rewards)
+    std_reward = np.std(episode_rewards)
+    return mean_reward, std_reward
+
+mean_reward, std_reward = evaluate_agent(env, max_steps, n_eval_episodes, Qtable_frozenlake, eval_seed)
+print(f"Mean reward={mean_reward} +/- {std_reward}")
